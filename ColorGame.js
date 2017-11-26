236cce44483319cb5cee8da964871307fd0bf433
spartanhero93 <spartanhero93@gmail.com>
2017-11-26 15:32:44 -0500

    Deleted free-space, and unnecessary comments

--- ColorGame.js
+++ ColorGame.js
@@ -39,45 +39,6 @@ function reset(){
     } h1.style.backgroundColor = ("steelblue");
 }
 
-
-
-
-
-
-
-
-
-
-
-// easyBtn.addEventListener ("click", function(){
-//     easyBtn.classList.add('selected');
-//     hardBtn.classList.remove('selected')
-//     numSquares = 3;
-//     colors = generateRandomColors(numSquares);
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-//     for (var i = 0; i < squares.length ; i++){
-//         if(colors[i]){
-//             squares[i].style.backgroundColor = colors[i];
-//         }else {
-//             squares[i].style.display = "none";
-//         }
-//     }
-// });
-// hardBtn.addEventListener("click", function () {
-//     hardBtn.classList.add('selected');
-//     easyBtn.classList.remove('selected');
-//     numSquares = 6;
-//     colors = generateRandomColors(numSquares);
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-//     for (var i = 0; i < squares.length; i++) {
-//             squares[i].style.backgroundColor = colors[i];
-//             squares[i].style.display = "block";
-//         }
-//     });
-
-
 resetButton.addEventListener("click", function () {
     reset();
 });
